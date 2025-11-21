63da1660c61b01f3fd4bba127083fa86229803a3
The-Deepak-Pandey <deepak.pandey@students.iiit.ac.in>
2025-11-21 22:32:24 +0530

    singint handler improved

--- signals.c
+++ signals.c
@@ -1,24 +1,28 @@
 #include "signals.h"
 #include "prompt.h"
 
-// Signal handler for SIGINT (Ctrl+C)
-void handle_sigint(int sig) { 
-    if(foreground_pid > 0) {
-        kill(foreground_pid, SIGINT); // terminate the foreground child process
+void handle_sigint(int sig) {
+    if (foreground_pid > 0) {
+        kill(foreground_pid, SIGKILL);
         foreground_pid = -1;
+        printf("\n");
+    } else {
+        printf("\n");
+        print_shell_prompt();
     }
-    printf("\n"); // move to new line after Ctrl+C
 }
 
 // to clean up zombie processes from background processes
 void handle_sigchld(int sig) {
     int status;
     pid_t pid;
+    int re_prompt = 0;
     while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {
         // find and remove from background_pids
         for (int i = 0; i < background_pid_count; i++) {
             if (background_pids[i] == pid) {
-                fprintf(stderr, "\nBackground process %d finished.\n", pid);
+                fprintf(stderr, "Background process %d finished.\n", pid);
+                re_prompt = 1;
                 // shift remaining pids to the left
                 for (int j = i; j < background_pid_count - 1; j++) {
                     background_pids[j] = background_pids[j+1];
@@ -28,5 +32,7 @@ void handle_sigchld(int sig) {
             }
         }
     }
-    print_shell_prompt();
+    if (re_prompt) {
+        print_shell_prompt();
+    }
 }
